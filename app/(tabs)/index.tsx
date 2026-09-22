1077dd2007daa2088a9cbd62aab9f83e762b0f2f
HRISHIKESH BADE <hrishikeshbade007@gmail.com>
2026-09-22 20:16:50 +0530

    basic routing added for tabs and auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,32 @@
 import "@/global.css";
 import { Text, View } from "react-native";
-
+import { Link, Stack } from "expo-router";
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
         Welcome to Nativewind!
       </Text>
+
+      <Link
+        href="/Onboarding"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        <Text className="text-lg text-white">Go to onboarding</Text>
+      </Link>
+      <Link
+        href="/(auth)/SignIn"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to SignIn
+      </Link>
+
+      <Link
+        href="/(auth)/SignUp"
+        className="mt-4 rounded bg-primary text-white p-4"
+      >
+        Go to SignUp
+      </Link>
     </View>
   );
 }
